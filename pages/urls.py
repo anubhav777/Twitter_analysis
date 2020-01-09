d884258cf66e5de3f74a13acadbe173246ca743d
Anubhav Thapa <magaranub@gmail.com>
2020-01-09 19:15:06 +0545

    routes for views

--- pages/urls.py
+++ pages/urls.py
@@ -11,16 +11,20 @@ router.register('pages',Displaypages)
 
 urlpatterns = [
    path('',include(router.urls)),
-
+   path('try/',modified_get),
+   path('newtry/',ad_data),
+   path('secondtry/<int:id>',insert_pages),
+   path('graph/',graph),
+   path('end/',date_end),
+   path("adsdis/",get_allads),
+   path("geo/",try_function),
+   path("adsdisplay/",ads_page_display),
+   path("getcountry/",country_list),
+   path("getsocial/",social_tracker),
+   path("adanalysis/",ads_analysis),
+   path('compare/',monthly_average),
+   path('blob/',blob)
 
   
 ]
 
-# def start_job():
-#     global job
-#     job=scheduler.add_job(timeout,'interval',minutes=30)
-#     try:
-#         scheduler.start()
-#     except :
-#         pass
-# start_job()
